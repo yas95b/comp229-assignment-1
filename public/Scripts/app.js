ca577bd23cbc7f257d0a1162bfa9625f51953c46
Yasaman Barzegar <yasaman.barzegar@yahoo.com>
2021-10-01 15:31:39 -0400

    init

---
+++ public/Scripts/app.js
@@ -0,0 +1,10 @@
+// IIFE -- Immediately invoked Function Expression
+
+(function(){
+    function Start()
+    {
+        console.log("App Started..");
+    }
+
+    window.addEventListener("load", Start);
+})();
