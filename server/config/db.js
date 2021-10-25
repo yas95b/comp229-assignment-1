34f23950b185cd7372a264eb126eec15de66fd14
yasaman barzegar <yasaman.barzegar@yahoo.com>
2021-10-25 16:37:40 -0400

    made changes to URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI":"mongodb://localhost/business_contact"
+    //"URI":"mongodb://localhost/business_contact"
+    "URI" :"mongodb+srv://yasaman:MC6pzU7MLkANDzl5@mongodbserver.r4wiu.mongodb.net/business_contact?retryWrites=true&w=majority"
 }
